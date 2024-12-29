13ac7460ce9cb4643038b323aba67a3b513bf50e
avivelkoni <avivb77@gmail.com>
2024-12-29 21:15:48 +0200

    part 3

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const fs = require('fs');
+const path = require('path');
 app.get('/video', (req, res) => {
-    console.log('Request received:', req.method, req.url);
-    console.log('Headers:', req.headers);
-    res.end();
-    });
-app.listen(3000);
+// Get the video’s actual location and size
+const videoPath = path.join(__dirname, 'public', 'video.mp4');
+const fileSize = fs.statSync(videoPath).size;
+// Extract the range requested by the browser
+const range = req.headers.range;
+const parts = range.substring(6).split('-');
+const start = parseInt(parts[0]);
+const chunk_size = 10 ** 6; // 1MB
+const end = Math.min(start + chunk_size, fileSize - 1);
+const file = fs.createReadStream(videoPath, { start, end });
+// Stream requested chunk
+const head = {
+'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+'Accept-Ranges': 'bytes',
+'Content-Length': chunk_size,
+'Content-Type': 'video/mp4',
+};
+res.writeHead(206, head);
+file.pipe(res);
+});
