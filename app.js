c6493f1c3e9edc60e8456de8bc383e282b7f207a
avivelkoni <avivb77@gmail.com>
2024-12-29 21:12:43 +0200

    part 2 (and remove the unessccery html)

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+app.get('/video', (req, res) => {
+    console.log('Request received:', req.method, req.url);
+    console.log('Headers:', req.headers);
+    res.end();
+    });
 app.listen(3000);
